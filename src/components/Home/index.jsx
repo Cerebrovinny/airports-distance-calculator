9b4c737dc980fea2083b59ce6d4ef3f7e89897bb
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-10-20 15:51:05 -0300

    added home page

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Airport Distance Calculator</h1>
+      <img src="/airplane.jpg" />
+
+      <h3>Distance:</h3>
+      <div id="map" />
+    </div>
+  );
+}
+
+export default Home;
