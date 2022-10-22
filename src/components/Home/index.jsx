62c4ec89850b8f45e1a5710fd31386fc4f6ebfd0
Cerebrovinny <vinny.cardoso96@gmail.com>
2022-10-22 12:20:01 -0300

    update home page

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,15 +1,31 @@
 import React from "react";
+import AutoComplete from "../AutoComplete";
+import Map from "../../components/Map";
+import {useLoadScript} from "@react-google-maps/api";
+const API_KEY = process.env.API_KEY;
 
 function Home() {
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: API_KEY,
+    });
+
+    if (loadError) return "Error loading maps";
+    if (!isLoaded) return "Loading Maps";
+
   return (
     <div>
       <h1>Airport Distance Calculator</h1>
       <img src="/airplane.jpg" />
-
+        {/*Pass autocomplete startMarker and endMarker to Map*/}
+        <AutoComplete label={"Start"} />
+        <AutoComplete label={"End"} />
       <h3>Distance:</h3>
-      <div id="map" />
+        <div id="map">
+        <Map />
+        </div>
     </div>
   );
 }
+
 
 export default Home;
